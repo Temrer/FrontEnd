a2dfe4e4f5f1b63ceb88bc6f21226b0724e36d77
Temrer <temrer14915@gmail.com>
2022-07-05 22:59:58 +0300

    added header and logo

---
+++ src/Page/App.js
@@ -0,0 +1,19 @@
+import logo from './Icons/RServiceLogo_transparent.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='Top-Banner'>
+        <div id = 'ie7'>
+          <div id='topBannerLogo-placeholder'>
+            <img src={logo} alt='logo transparent' id='BannerLogo'></img>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
